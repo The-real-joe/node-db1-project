3064933fbf2c6d2ad6dfcb7525a82f2f2b087c2d
The-real-joe <j.boucher690@gmail.com>
2024-02-21 18:41:46 -0500

    done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,27 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // select * from accounts-- Basically what the return line is saying. only because becaus of knex and how ot is linked up to the data and being imported in here
+  return db('accounts');
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  //insert into accounts (name, badget) values ('foo'. 1000); --WHAT the CODE BELOW IS ESSENTIALLY SAYING
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  await db('accounts').where('id', id).update(account)
+  return getById(id);
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+ return db('accounts').where('id', id).del()
 }
 
 module.exports = {
